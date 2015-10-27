192d1d56f3034dd35ff73f58e402ca0a256d78d2
tykim <goddes4@gmail.com>
2015-10-27 21:47:07 +0900

    fix to use space indentation

--- src/main/java/taeyo/sample/netty/NioMessageSender.java
+++ src/main/java/taeyo/sample/netty/NioMessageSender.java
@@ -29,130 +29,131 @@ import java.util.stream.Collectors;
 @Setter
 public class NioMessageSender {
 
-	private static final int RETRY_DELAY_TIME = 10;
+    private static final int RETRY_DELAY_TIME = 10;
 
-	private final Bootstrap bootstrap = new Bootstrap();
+    private final Bootstrap bootstrap = new Bootstrap();
 
-	private final String serverIP;
-	private final int serverPort;
+    private final String serverIP;
+    private final int serverPort;
 
-	private SuccessCallback<Map<String, String>> successCallback;
-	private FailureCallback failureCallback;
-	private EventLoopGroup workerGroup;
-	private int messageTimeoutMilliseconds = 3000;
+    private SuccessCallback<Map<String, String>> successCallback;
+    private FailureCallback failureCallback;
+    private EventLoopGroup workerGroup;
+    private int messageTimeoutMilliseconds = 3000;
 
-	public NioMessageSender(String serverIP, int serverPort) {
-		this.serverIP = serverIP;
-		this.serverPort = serverPort;
-	}
-
-	public void init() {
-    	if (workerGroup == null) {
-    		workerGroup = new NioEventLoopGroup(1);
-    	}
-
-    	bootstrap.group(workerGroup)
-    		.channel(NioSocketChannel.class)
-    		.option(ChannelOption.TCP_NODELAY, true)
-    		.option(ChannelOption.SO_KEEPALIVE, true)
-    		.handler(new ChannelInitializer<SocketChannel>() {
-
-				@Override
-				protected void initChannel(SocketChannel ch) throws Exception {
-					log.info("ch : " + ch);
-					ch.pipeline().addLast(
-							new StringEncoder(),
-							new MapToStringEncoder(),
-							new LineBasedFrameDecoder(8192),
-							new ReadTimeoutHandler(messageTimeoutMilliseconds, TimeUnit.MILLISECONDS),
-							new StringDecoder(),
-							new SimpleChannelInboundHandler<String>() {
-
-								private Map<String, String> map = new HashMap<>();
-
-								@Override
-								public void channelActive(ChannelHandlerContext ctx) throws Exception {
-									log.debug("Channel has been active.");
-								}
-
-								@Override
-								public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-									if (cause instanceof ReadTimeoutException) {
-										if (!map.isEmpty()) {
-											try {
-												successCallback.onSuccess(map);
-											} catch (RuntimeException e) {
-												e.printStackTrace();
-											}
-										} else {
-											failureCallback.onFailure(new NotReceivedMessageException("Not received message"));
-										}
-									} else {
-										failureCallback.onFailure(cause);
-									}
-									ctx.close();
-								}
-
-								@Override
-								protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-									String[] split = msg.split("=");
-									map.put(split[0], split[1]);
-								}
-							});
-				}
-			});
+    public NioMessageSender(String serverIP, int serverPort) {
+        this.serverIP = serverIP;
+        this.serverPort = serverPort;
     }
 
-	public void send(Map<String, String> outMap) {
-		log.debug("Attempts a new connection to {} TCP Server.", serverIP);
+    public void init() {
+        if (workerGroup == null) {
+            workerGroup = new NioEventLoopGroup(1);
+        }
 
-		ChannelFuture future = bootstrap.connect(serverIP, serverPort).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+        bootstrap.group(workerGroup)
+            .channel(NioSocketChannel.class)
+            .option(ChannelOption.TCP_NODELAY, true)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .handler(new ChannelInitializer<SocketChannel>() {
+
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    log.info("ch : " + ch);
+                    ch.pipeline().addLast(
+                        new StringEncoder(),
+                        new MapToStringEncoder(),
+                        new LineBasedFrameDecoder(8192),
+                        new ReadTimeoutHandler(messageTimeoutMilliseconds, TimeUnit.MILLISECONDS),
+                        new StringDecoder(),
+                        new SimpleChannelInboundHandler<String>() {
+
+                            private Map<String, String> map = new HashMap<>();
+
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                log.debug("Channel has been active.");
+                            }
+
+                            @Override
+                            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                                if (cause instanceof ReadTimeoutException) {
+                                    if (!map.isEmpty()) {
+                                        try {
+                                            successCallback.onSuccess(map);
+                                        } catch (RuntimeException e) {
+                                            e.printStackTrace();
+                                        }
+                                    } else {
+                                        failureCallback.onFailure(new NotReceivedMessageException("Not received message"));
+                                    }
+                                } else {
+                                    failureCallback.onFailure(cause);
+                                }
+                                ctx.close();
+                            }
+
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+                                String[] split = msg.split("=");
+                                map.put(split[0], split[1]);
+                            }
+                        });
+                }
+            });
+    }
+
+    public void send(Map<String, String> outMap) {
+        log.debug("Attempts a new connection to {} TCP Server.", serverIP);
+
+        ChannelFuture future = bootstrap.connect(serverIP, serverPort).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
         future.addListener((ChannelFuture channelFuture) -> {
-			if (channelFuture.isSuccess()) {
-				log.debug("Success!!!");
+            if (channelFuture.isSuccess()) {
+                log.debug("Success!!!");
 
-				Channel ch = channelFuture.channel();
-				ch.closeFuture().addListener((ChannelFuture cf) -> log.debug("Channel Close!!!"));
-				ch.writeAndFlush(outMap).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
-			}
-		});
-	}
+                Channel ch = channelFuture.channel();
+                ch.closeFuture().addListener((ChannelFuture cf) -> log.debug("Channel Close!!!"));
+                ch.writeAndFlush(outMap).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+            }
+        });
+    }
 
-	public void addCallback(SuccessCallback<Map<String, String>> successCallback, FailureCallback failureCallback) {
-		this.successCallback = successCallback;
-		this.failureCallback = failureCallback;
-	}
+    public void addCallback(SuccessCallback<Map<String, String>> successCallback, FailureCallback failureCallback) {
+        this.successCallback = successCallback;
+        this.failureCallback = failureCallback;
+    }
 
     public static void main(String[] args) throws InterruptedException {
-		int WORKER_COUNT = 10;
+        int WORKER_COUNT = 10;
 
-		EventLoopGroup workerGroup = new NioEventLoopGroup();
-		CountDownLatch countDownLatch = new CountDownLatch(WORKER_COUNT);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        CountDownLatch countDownLatch = new CountDownLatch(WORKER_COUNT);
 
         NioMessageSender messageSender = new NioMessageSender("127.0.0.1", 9001);
-		messageSender.setWorkerGroup(workerGroup);
-		messageSender.setMessageTimeoutMilliseconds(5000);
+        messageSender.setWorkerGroup(workerGroup);
+        messageSender.setMessageTimeoutMilliseconds(5000);
         messageSender.init();
-		messageSender.addCallback(inMap -> {
-			log.info(inMap.entrySet()
-					.stream()
-					.map(entry -> entry.getKey() + "=" + entry.getValue())
-					.collect(Collectors.joining(", ", "[", "]")));
-			countDownLatch.countDown();
-		}, ex -> {
-			log.error("{}", ex.getMessage());
-			countDownLatch.countDown();
-		});
+        messageSender.addCallback(inMap -> {
+            log.info(inMap.entrySet()
+                    .stream()
+                    .map(entry -> entry.getKey() + "=" + entry.getValue())
+                    .collect(Collectors.joining(", ", "[", "]")));
+            countDownLatch.countDown();
+        }, ex -> {
+            log.error("{}", ex.getMessage());
+            countDownLatch.countDown();
+        });
 
-		for (int i = 0; i < WORKER_COUNT; i++) {
-			Map<String, String> outMap = new HashMap<>();
-			outMap.put("KEY1", "VALUE1-" + i);
-			outMap.put("KEY2", "VALUE2-" + i);
-			outMap.put("KEY3", "VALUE3-" + i);
-			messageSender.send(outMap);
-		}
+        for (int i = 0; i < WORKER_COUNT; i++) {
+            Map<String, String> outMap = new HashMap<>();
+            outMap.put("KEY1", "VALUE1-" + i);
+            outMap.put("KEY2", "VALUE2-" + i);
+            outMap.put("KEY3", "VALUE3-" + i);
+            messageSender.send(outMap);
+        }
 
-		countDownLatch.await();
-		workerGroup.shutdownGracefully();
-	}
+        countDownLatch.await();
+        workerGroup.shutdownGracefully();
+    }
 }
+
